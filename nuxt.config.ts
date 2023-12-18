a04c622a96f97648afbd125f0954bf728227e06e
Lucas Verissimo <verissimo.lucas1994@gmail.com>
2023-12-18 20:14:42 -0300

    Optimize Google searches

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,11 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'UDev',
+      title: 'Convert px, rem, em, pt, mm, cm',
       meta: [
         { name: 'UDev', content: 'Website for web developers. Convert css units.' },
-        { name: 'description', content: 'Effortlessly convert CSS units with our online tool. Seamlessly switch between pixels, REM, centimeters, and more. Optimize your web development workflow by quickly transforming measurements to meet your design needs. Try our CSS unit converter now!' }
+        { name: 'description', content: 'Effortlessly convert CSS units with our online tool. Seamlessly switch between pixels, REM, EM, cm, mm and pt. Optimize your web development workflow by quickly transforming measurements to meet your design needs. Try our CSS unit converter now!' },
+        { name: 'keywords', content: 'pixel pixels rem em centimeters cm millimeter mm pt point' }
       ],
       htmlAttrs: {
         lang: 'en'
@@ -25,9 +26,17 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/image',
     '@nuxtjs/robots',
+    '@nuxtjs/sitemap',
+    '@vite-pwa/nuxt'
   ],
   robots: {
     UserAgent: '*',
-    Disallow: ''
+    Disallow: '/',
+    Sitemap: 'https://4udev.vercel.app/sitemap.xml',
+  },
+  sitemap: {
+    path: '/sitemap.xml',
+    hostname: 'https://4udev.vercel.app',
+    generate: true,
   },
 })
